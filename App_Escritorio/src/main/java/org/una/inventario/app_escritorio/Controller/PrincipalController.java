03bb4206080f53ff8ef9fd99a7e10eef42b80051
Josselyn Mora S <jossems.1012@gmail.com>
2021-10-26 23:36:55 -0600

    Ajustes

--- App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
+++ App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
@@ -3,7 +3,7 @@ package org.una.inventario.app_escritorio.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class PrincipalView extends Application {
+public class PrincipalController extends Application {
 
     public static void main(String[] args) {
         launch(args);
