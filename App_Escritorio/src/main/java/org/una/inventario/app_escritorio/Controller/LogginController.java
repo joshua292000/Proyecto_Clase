fbd57e4771747f4dd76279ae59516482099566c1
Josselyn Mora S <jossems.1012@gmail.com>
2021-10-27 08:26:29 -0600

    Loggin

--- App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/LogginController.java
+++ App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/LogginController.java
@@ -1,16 +1,22 @@
 package org.una.inventario.app_escritorio.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogginController extends Controller implements Initializable {
-
     @FXML
-    private Label lblHola;
+    public JFXTextField txtUsuario;
+    public JFXPasswordField txtContrasenia;
+    public JFXButton btnIngresar;
+
 
     @Override
     public void initialize() {
@@ -20,5 +26,8 @@ public class LogginController extends Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void OnActionbtnIngresar(ActionEvent actionEvent) {
     }
 }
