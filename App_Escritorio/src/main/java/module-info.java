a7dae1988029e2ddc98e84d55d599637bf9ef83a
joshua292000 <joshua292000@gmail.com>
2021-10-26 22:27:07 -0600

    dependencias

--- App_Escritorio/src/main/java/module-info.java
+++ App_Escritorio/src/main/java/module-info.java
@@ -9,8 +9,13 @@ module org.una.inventario.app_escritorio {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.logging;
+    requires com.jfoenix;
+    requires lombok;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
 
-    opens org.una.inventario.app_escritorio to javafx.fxml;
+    opens org.una.inventario.app_escritorio to javafx.fxml, javafx.base;
     exports org.una.inventario.app_escritorio;
     exports org.una.inventario.app_escritorio.Views;
     opens org.una.inventario.app_escritorio.Views to javafx.fxml;
