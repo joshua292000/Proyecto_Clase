f345d6b83db80a0d2f3c42e4cacb9e94e627bdae
joshua292000 <joshua292000@gmail.com>
2021-10-27 20:13:10 -0600

    consultas

--- App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
+++ App_Escritorio/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
@@ -15,7 +15,10 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.layout.AnchorPane;
 import org.una.inventario.app_escritorio.DTO.ActivoDTO;
 import org.una.inventario.app_escritorio.DTO.AuthenticationResponse;
+import org.una.inventario.app_escritorio.DTO.MarcaDTO;
+import org.una.inventario.app_escritorio.DTO.ProveedoresDTO;
 import org.una.inventario.app_escritorio.Service.AutenticacionService;
+import org.una.inventario.app_escritorio.Service.ConsultasService;
 import org.una.inventario.app_escritorio.Service.ReporteService;
 import org.una.inventario.app_escritorio.Util.FlowController;
 
@@ -34,7 +37,6 @@ public class PrincipalController extends Controller implements Initializable {
     public JFXRadioButton cbAscendente;
     @FXML
     private AnchorPane APFondo;
-
     @FXML
     public JFXRadioButton cbMarca;
     @FXML
@@ -46,7 +48,7 @@ public class PrincipalController extends Controller implements Initializable {
     @FXML
     private JFXButton btnVisualizarReporte;
     @FXML
-    private DatePicker  dtpFInicio;
+    private DatePicker dtpFInicio;
     @FXML
     private DatePicker dtpFFinal;
 
@@ -55,14 +57,21 @@ public class PrincipalController extends Controller implements Initializable {
     @FXML
     private ComboBox cbxPrueba = new ComboBox(options);
 
+    private int tipo=0;
+    private int AntDec =0;
+
 
     public void OnActionbtnGenerarReporte(ActionEvent actionEvent) throws IOException, InterruptedException {
-
-        List<ActivoDTO> activos = ReporteService.Reporte();
-        for(ActivoDTO activo:activos){
-            options.add(activo.getId() +"-"+ activo.getNombre());
+        int id = 1;
+        if(tipo==2 && AntDec==2){
+            List<ActivoDTO> activo = ConsultasService.ObtenerActivo1(id,dtpFInicio.getValue(),dtpFFinal.getValue());
+        }else if(tipo==2 && AntDec==1){
+            List<ActivoDTO> activo = ConsultasService.ObtenerActivo2(id,dtpFInicio.getValue(),dtpFFinal.getValue());
+        }else if(tipo==1 && AntDec==1){
+            List<ActivoDTO> activo = ConsultasService.ObtenerActivo3(id,dtpFInicio.getValue(),dtpFFinal.getValue());
+        }else if(tipo==1 && AntDec==2){
+            List<ActivoDTO> activo = ConsultasService.ObtenerActivo4(id,dtpFInicio.getValue(),dtpFFinal.getValue());
         }
-        cbxPrueba.setItems(options);
 
     }
 
@@ -88,15 +97,43 @@ public class PrincipalController extends Controller implements Initializable {
 
     }
 
-    public void OnActioncbProveedor(ActionEvent actionEvent) {
+    public void OnActioncbProveedor(ActionEvent actionEvent) throws IOException, InterruptedException {
+        tipo = 1;
+        OpcionPM(tipo);
     }
 
-    public void OnActioncbMarca(ActionEvent actionEvent) {
+    public void OnActioncbMarca(ActionEvent actionEvent) throws IOException, InterruptedException {
+        tipo = 2;
+        OpcionPM(tipo);
     }
 
     public void OnActioncbDescendente(ActionEvent actionEvent) {
+        AntDec =1;
     }
 
     public void OnActioncbAscendente(ActionEvent actionEvent) {
+        AntDec =2;
+    }
+
+    public void OpcionPM(int tipo) throws IOException, InterruptedException {
+        if(tipo == 1){
+            List<ProveedoresDTO> proveedor = ConsultasService.ProveeCBX();
+            cbxPrueba.getItems().removeAll();
+            for(ProveedoresDTO proveedores:proveedor){
+                options.add(proveedores.getId() +"-"+ proveedores.getNombre());
+            }
+            cbxPrueba.setDisable(false);
+            cbxPrueba.setItems(options);
+
+        }else if(tipo == 2){
+            List<MarcaDTO> marca = ConsultasService.MarcaCBX();
+            cbxPrueba.getItems().removeAll();
+            for(MarcaDTO marcas:marca){
+                options.add(marcas.getId() +"-"+ marcas.getNombre());
+            }
+            cbxPrueba.setDisable(false);
+            cbxPrueba.setItems(options);
+
+        }
     }
 }
