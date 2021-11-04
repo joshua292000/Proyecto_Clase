beb2b8e597e48de3ae78fd24324b95f4c282a38d
Josselyn Mora S <jossems.1012@gmail.com>
2021-11-04 17:08:27 -0600

    Validar la información (Continentes) y mostrar datos(corregido)

--- Examen/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
+++ Examen/src/main/java/org/una/inventario/app_escritorio/Controller/PrincipalController.java
@@ -79,90 +79,107 @@ public class PrincipalController extends Controller implements Initializable {
         CSVParser conPuntoYComa = new CSVParserBuilder().withSeparator(';').build();
         csvReader = new CSVReaderBuilder(archCSV).withCSVParser(conPuntoYComa).build();
         String[] fila = null;
+        int num=0;
         if (filecsv != null) {
             while ((fila = csvReader.readNext()) != null) {
+                if (num < 1) {
+                    num = 1;
+                } else {
+                    System.out.println(fila[0]
+                            + " | " + fila[1]
+                            + " |  " + fila[2]
+                            + " |   " + fila[3]
+                            + " |    " + fila[4]
+                            + " |      " + fila[5]
+                            + " |       " + fila[6]
+                            + " |        " + fila[7]
+                            + " |         " + fila[8]
+                            + " |           " + fila[9]
+                            + " |             " + fila[10]
+                    );
+                    //options.add(new ActivosDTO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9],fila[10]));
+                    boolean Validarfila2 = fila[2].matches("[+-]?\\d*(\\.\\d+)?");
+                    boolean Validarfila4 = fila[4].matches("[+-]?\\d*(\\.\\d+)?");
 
-             //System.out.println("num"+Validarfila2);
-             //System.out.println("telef"+Validarfila4);
-                System.out.println(fila[0]
-                        + " | " + fila[1]
-                        + " |  " + fila[2]
-                        + " |   " + fila[3]
-                        + " |    " + fila[4]
-                        + " |      " + fila[5]
-                        + " |       " + fila[6]
-                        + " |        " + fila[7]
-                        + " |         " + fila[8]
-                        + " |           " + fila[9]
-                        + " |             " + fila[10]
-                );
-                //options.add(new ActivosDTO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9],fila[10]));
-                boolean Validarfila2 = fila[2].matches("[+-]?\\d*(\\.\\d+)?");
-                boolean Validarfila4 = fila[4].matches("[+-]?\\d*(\\.\\d+)?");
+                    if (Validarfila2 == false) {
+                        int seleccion = JOptionPane.showOptionDialog(null, "El campo con la siguiente información <<" + fila[2] + ">> posee letras, favor solo ingresar numeros, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss, opcionemss[0]);
+                        System.out.println("Favor revisar la fila del numero, ingrese solo números");
+                        switch (seleccion) {
+                            case 0:
+                                System.out.println("Hacer cambios");
+                                String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<" + fila[2] + ">>", "Error!", JOptionPane.ERROR_MESSAGE);
+                                System.out.println("Cambios " + respuesta);
+                                fila[2] = respuesta;
+                                break;
+                            case 1:
+                                fila[2] = null;
+                                System.out.println("Descartar");
+                                break;
+                        }
 
-                if(Validarfila2==false){
-                   int seleccion = JOptionPane.showOptionDialog(null,"El campo con la siguiente información <<"+fila[2]+">> posee letras, favor solo ingresar numeros, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss,opcionemss[0]);
-                    System.out.println("Favor revisar la fila del numero, ingrese solo números");
-                    switch (seleccion){
-                        case 0:
-                            System.out.println("Hacer cambios");
-                            String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<"+fila[2]+">>", "Error!", JOptionPane.ERROR_MESSAGE);
-                            System.out.println("Cambios "+respuesta);
-                            fila[2]=respuesta;
-                            break;
-                        case 1:
-                            fila[2]=null;
-                            System.out.println("Descartar");
-                            break;
                     }
-
-                }
-                if(Validarfila4==false){
-                    int seleccion =JOptionPane.showOptionDialog(null,"El campo <<"+fila[4]+">> posee letras, favor solo ingresar numeros sin guiones ni letras, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss,opcionemss[0]);
-                    System.out.println("Favor revisar la fila de teléfono, ingrese solo números sin guiones de por medio");
-                    switch (seleccion){
-                        case 0:
-                            String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<"+fila[4]+">>", "Error!", JOptionPane.ERROR_MESSAGE);
-                            fila[4]=respuesta;
-                            System.out.println("Nuevo Campo Fila[4] "+fila[4]);
-                            break;
-                        case 1:
-                            fila[4]=null;
-                            System.out.println("Descartar");
-                            break;
+                    if (Validarfila4 == false) {
+                        int seleccion = JOptionPane.showOptionDialog(null, "El campo <<" + fila[4] + ">> posee letras, favor solo ingresar numeros sin guiones ni letras, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss, opcionemss[0]);
+                        System.out.println("Favor revisar la fila de teléfono, ingrese solo números sin guiones de por medio");
+                        switch (seleccion) {
+                            case 0:
+                                String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<" + fila[4] + ">>", "Error!", JOptionPane.ERROR_MESSAGE);
+                                fila[4] = respuesta;
+                                System.out.println("Nuevo Campo Fila[4] " + fila[4]);
+                                break;
+                            case 1:
+                                fila[4] = null;
+                                System.out.println("Descartar");
+                                break;
+                        }
                     }
-                }
-                if(isDateValid(fila[6])==false){
-                    int seleccion =JOptionPane.showOptionDialog(null,"El campo <<"+fila[6]+">> posee un formato distinto de fecha, favor solo ingresar en el siguiente formato <<dd/MM/yyyy>>, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss,opcionemss[0]);
-                    switch (seleccion){
-                        case 0:
-                            System.out.println("Hacer cambios");
-                            String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<"+fila[6]+">>", "Error!", JOptionPane.ERROR_MESSAGE);
-                            System.out.println("Cambios "+respuesta);
-                            fila[6]=respuesta;
-                            break;
-                        case 1:
-                            fila[6]=null;
-                            System.out.println("Descartar");
-                            break;
+                    if (isDateValid(fila[6]) == false) {
+                        int seleccion = JOptionPane.showOptionDialog(null, "El campo <<" + fila[6] + ">> posee un formato distinto de fecha, favor solo ingresar en el siguiente formato <<dd/MM/yyyy>>, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss, opcionemss[0]);
+                        switch (seleccion) {
+                            case 0:
+                                System.out.println("Hacer cambios");
+                                String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<" + fila[6] + ">>", "Error!", JOptionPane.ERROR_MESSAGE);
+                                System.out.println("Cambios " + respuesta);
+                                fila[6] = respuesta;
+                                break;
+                            case 1:
+                                fila[6] = null;
+                                System.out.println("Descartar");
+                                break;
+                        }
                     }
-                }
-                if(isDateValid(fila[10])==false){
-                    int seleccion = JOptionPane.showOptionDialog(null,"El campo <<"+fila[10]+">> posee un formato distinto de fecha, favor solo ingresar en el siguiente formato <<dd/MM/yyyy>>, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss,opcionemss[0]);
-                    switch (seleccion){
-                        case 0:
-                            System.out.println("Hacer cambios");
-                            String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<"+fila[10]+">>", "Error!", JOptionPane.ERROR_MESSAGE);
-                            System.out.println("Cambios "+respuesta);
-                            fila[10]=respuesta;
-                            break;
-                        case 1:
-                            fila[10]=null;
-                            System.out.println("Descartar");
-                            break;
+                    if(fila[7]!=null||fila[7]!="1"||fila[7]!="2"||fila[7]!="3"||fila[7]!="4"||fila[7]!="5"||fila[7]!="6"){
+                        int seleccion = JOptionPane.showOptionDialog(null,"El campo <<"+fila[7]+">> favor poner un valor del 1 al 6,el cual corresponde respectivamente: 1.América 2.Europa 3.Asia 4.Oceanía 5.África 6.Antártida ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss,opcionemss[0]);
+                        switch (seleccion){
+                            case 0:
+                                System.out.println("Hacer cambios");
+                                String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<"+fila[7]+">>", "Error!", JOptionPane.ERROR_MESSAGE);
+                                System.out.println("Cambios "+respuesta);
+                                fila[7]=respuesta;
+                                break;
+                            case 1:
+                                fila[7]=null;
+                                System.out.println("Descartar");
+                                break;
+                        }
                     }
+                    if (isDateValid(fila[10]) == false) {
+                        int seleccion = JOptionPane.showOptionDialog(null, "El campo <<" + fila[10] + ">> posee un formato distinto de fecha, favor solo ingresar en el siguiente formato <<dd/MM/yyyy>>, ¿Qué desea hacer?", "Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionemss, opcionemss[0]);
+                        switch (seleccion) {
+                            case 0:
+                                System.out.println("Hacer cambios");
+                                String respuesta = JOptionPane.showInputDialog(null, "Escriba correctamente el campo <<" + fila[10] + ">>", "Error!", JOptionPane.ERROR_MESSAGE);
+                                System.out.println("Cambios " + respuesta);
+                                fila[10] = respuesta;
+                                break;
+                            case 1:
+                                fila[10] = null;
+                                System.out.println("Descartar");
+                                break;
+                        }
+                    }
+                    options.add(new ActivosDTO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9], fila[10]));
                 }
-                options.add(new ActivosDTO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6], fila[7], fila[8], fila[9],fila[10]));
             }
         }
     }
